c09bac99049bc208c2f560d5c1e8e140135088cd
nik <nikita.moiseienko@onix-systems.com>
2021-01-14 19:46:52 +0200

    https://www.digitalocean.com/community/tutorials/how-to-scrape-a-website-using-node-js-and-puppeteer-ru

--- pageScraper.js
+++ pageScraper.js
@@ -4,6 +4,45 @@ const scraperObject = {
         const page = await browser.newPage();
         console.log(`Navigating to ${this.url}...`);
         await page.goto(this.url);
+        // Wait for the required DOM to be rendered
+        await page.waitForSelector('.page_inner');
+        // Get the link to all the required books
+        const urls = await page.$$eval('section ol > li', (links) => {
+            // Make sure the book to be scraped is in stock
+            links = links.filter((link) => link.querySelector('.instock.availability > i').textContent !== 'In stock');
+            // Extract the links from the data
+            links = links.map((el) => el.querySelector('h3 > a').href);
+            return links;
+        });
+        console.log(urls);
+
+        // Loop through each of those links, open a new page instance and get the relevant data from them
+        const pagePromise = (link) => new Promise(async (resolve, reject) => {
+            const dataObj = {};
+            const newPage = await browser.newPage();
+            await newPage.goto(link);
+            dataObj.bookTitle = await newPage.$eval('.product_main > h1', (text) => text.textContent);
+            dataObj.bookPrice = await newPage.$eval('.price_color', (text) => text.textContent);
+            dataObj.noAvailable = await newPage.$eval('.instock.availability', (text) => {
+                // Strip new line and tab spaces
+                text = text.textContent.replace(/(\r\n\t|\n|\r|\t)/gm, '');
+                // Get the number of stock available
+                const regexp = /^.*\((.*)\).*$/i;
+                const stockAvailable = regexp.exec(text)[1].split(' ')[0];
+                return stockAvailable;
+            });
+            dataObj.imageUrl = await newPage.$eval('#product_gallery img', (img) => img.src);
+            dataObj.bookDescription = await newPage.$eval('#product_description', (div) => div.nextSibling.nextSibling.textContent);
+            dataObj.upc = await newPage.$eval('.table.table-striped > tbody > tr > td', (table) => table.textContent);
+            resolve(dataObj);
+            await newPage.close();
+        });
+
+        for (link in urls) {
+            const currentPageData = await pagePromise(urls[link]);
+            // scrapedData.push(currentPageData);
+            console.log(currentPageData);
+        }
     },
 };
 
